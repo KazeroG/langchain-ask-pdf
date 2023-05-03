cb5d4debe681dc018e5a6b06ec6b28b5c52da24b
KazeroG <17680194+KazeroG@users.noreply.github.com>
2023-05-03 17:39:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from dotenv import load_dotenv
+from io import BytesIO
+import requests
 import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
@@ -8,45 +9,44 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
 
-
 def main():
-    load_dotenv()
     st.set_page_config(page_title="Ask your PDF")
     st.header("Ask your PDF 💬")
     
-    # upload file
-    pdf = st.file_uploader("Upload your PDF", type="pdf")
+    # load the PDF file
+    url = 'https://www.example.com/example.pdf'
+    response = requests.get(url)
+    pdf = BytesIO(response.content)
     
     # extract the text
-    if pdf is not None:
-      pdf_reader = PdfReader(pdf)
-      text = ""
-      for page in pdf_reader.pages:
+    pdf_reader = PdfReader(pdf)
+    text = ""
+    for page in pdf_reader.pages:
         text += page.extract_text()
-        
-      # split into chunks
-      text_splitter = CharacterTextSplitter(
+
+    # split into chunks
+    text_splitter = CharacterTextSplitter(
         separator="\n",
         chunk_size=1000,
         chunk_overlap=200,
         length_function=len
-      )
-      chunks = text_splitter.split_text(text)
+    )
+    chunks = text_splitter.split_text(text)
       
-      # create embeddings
-      embeddings = OpenAIEmbeddings()
-      knowledge_base = FAISS.from_texts(chunks, embeddings)
+    # create embeddings
+    embeddings = OpenAIEmbeddings()
+    knowledge_base = FAISS.from_texts(chunks, embeddings)
       
-      # show user input
-      user_question = st.text_input("Ask a question about your PDF:")
-      if user_question:
+    # show user input
+    user_question = st.text_input("Ask a question about your PDF:")
+    if user_question:
         docs = knowledge_base.similarity_search(user_question)
         
         llm = OpenAI()
         chain = load_qa_chain(llm, chain_type="stuff")
         with get_openai_callback() as cb:
-          response = chain.run(input_documents=docs, question=user_question)
-          print(cb)
+            response = chain.run(input_documents=docs, question=user_question)
+            print(cb)
            
         st.write(response)
     
